d2673b2b585a9d040b1e5f9b7a6c50916a10e20c
koolong <koomingchin@gmail.com>
2023-02-11 17:50:03 +0800

    Update axis_chart_scaffold_widget.dart

--- lib/src/chart/base/axis_chart/axis_chart_scaffold_widget.dart
+++ lib/src/chart/base/axis_chart/axis_chart_scaffold_widget.dart
@@ -116,18 +116,20 @@ class AxisChartScaffoldWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Container(
-            width: 3000,
-            padding: EdgeInsets.all(8),
-            margin: EdgeInsets.only(left: 100),
-            child: LayoutBuilder(
-              builder: (context, constraints) {
-                return Stack(
-                  children: stackWidgets(constraints),
-                );
-              },
+        Padding(
+          padding: EdgeInsets.only(left: 100),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Container(
+              width: 3000,
+              padding: EdgeInsets.all(8),
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  return Stack(
+                    children: stackWidgets(constraints),
+                  );
+                },
+              ),
             ),
           ),
         ),

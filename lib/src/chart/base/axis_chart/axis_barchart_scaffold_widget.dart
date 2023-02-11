2676a6d607e2e4ecaac777a12e9a905d33f76131
koo <koomingchin@gmail.com>
2023-02-11 20:01:49 +0800

    revert

--- lib/src/chart/base/axis_chart/axis_barchart_scaffold_widget.dart
+++ lib/src/chart/base/axis_chart/axis_barchart_scaffold_widget.dart
@@ -69,12 +69,7 @@ class AxisBarchartScaffoldWidget extends StatelessWidget {
   List<Widget> stackWidgets(BoxConstraints constraints) {
     final widgets = <Widget>[
       Container(
-        margin: EdgeInsets.only(
-          left: 0,
-          right: data.titlesData.allSidesPadding.right,
-          top: data.titlesData.allSidesPadding.top,
-          bottom: data.titlesData.allSidesPadding.bottom,
-        ),
+        margin: data.titlesData.allSidesPadding,
         decoration: BoxDecoration(
           border: data.borderData.isVisible() ? data.borderData.border : null,
         ),
